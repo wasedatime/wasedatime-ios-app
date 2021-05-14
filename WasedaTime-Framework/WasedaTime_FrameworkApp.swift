31031cfab355923a8e68479e1eff238f5fc87c4d
ronin207 <takumi.ot0911@gmail.com>
2021-05-14 08:52:03 +0800

    Initial Commit

---
+++ WasedaTime-Framework/WasedaTime_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasedaTime_FrameworkApp.swift
+//  WasedaTime-Framework
+//
+//  Created by Takumi Otsuka on 14/5/21.
+//
+
+import SwiftUI
+
+@main
+struct WasedaTime_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
